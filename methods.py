52dd63fb4f5f04c37c73c378edea83ec742e97c8
Rok Grgec <rok.grgec@gmail.com>
2024-08-12 15:59:53 +0000

    Updated tests, fixed formating

--- methods.py
+++ methods.py
@@ -5,29 +5,32 @@ from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 def load_data() -> tp.List[str]:
-    '''
+    """
     looad_data loads the 20-newsgroups dataset
-    '''
+    """
     try:
-        newsgroups = fetch_20newsgroups(subset='all')
+        newsgroups = fetch_20newsgroups(subset="all")
         data = newsgroups.data
         return data
     except Exception as e:
         raise e
 
+
 newsgroups = load_data()
 
 documents = newsgroups
 
+
 def prepare_data() -> tp.Tuple:
-    '''
+    """
     prepare_data computes TF-IDF matrix for documents and returns
     a tuple of tfidf_matrix and vectorizer
-    '''
+    """
     # Compute TF-IDF matrix for documents
     try:
-        vectorizer = TfidfVectorizer(stop_words='english')
+        vectorizer = TfidfVectorizer(stop_words="english")
         # example of tfidf_matrix
         # array(['and', 'document', 'one',
         # 'second', 'the', 'third','this'], ...)
@@ -37,13 +40,14 @@ def prepare_data() -> tp.Tuple:
     except Exception as e:
         raise e
 
+
 def rank_doc(input_str: str) -> pd.DataFrame:
-    '''
+    """
     rank_doc takes input_word str that is being searched through
     documents and it is ranked with Cosine Similarity score
 
     method returns data as dataframe
-    '''
+    """
     try:
         tfidf_matrix, vectorizer = prepare_data()
 
@@ -55,24 +59,24 @@ def rank_doc(input_str: str) -> pd.DataFrame:
 
         # Rank Documents based on Cosine Similarity
         ranked_indices = np.argsort(cosine_similarities)[::-1]
-        ranked_documents = [documents[i] for i in ranked_indices]
+        ranked_docs = [documents[i] for i in ranked_indices]
         ranked_scores = [cosine_similarities[i] for i in ranked_indices]
 
         # Convert results to a DataFrame for better visualization
-        results_df = pd.DataFrame({
-            'Document': ranked_documents[:5],
-            'Score': ranked_scores[:5]
-        })
+        results_df = pd.DataFrame(
+            {"Document": ranked_docs[:5], "Score": ranked_scores[:5]}
+        )
 
         return results_df
     except Exception as e:
         return {"message": e}
 
+
 def add_document_text(document) -> dict:
-    '''
+    """
     add_document_text add_document_text takes str as input param
     and adds that str to the list of documents
-    '''
+    """
     # Adds document as str to the list of documents
     try:
         documents.append(document.text)
--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,15 +1,26 @@
 from methods import documents, load_data, prepare_data, rank_doc, add_document_text
 from main import Document
-from mock import patch
 
 
 def test_load_data():
     data = load_data()
     assert data
 
+
 def test_prepare_data():
     tfidf_matrix, vectorizer = prepare_data()
     assert vectorizer
+
+
+def test_rank_doc():
+    data = rank_doc("problem")
+    assert len(data.Score) == 5
+
+    dummy_doc = Document(text="TEst, test, test ,test ,test, test.")
+    add_document_text(dummy_doc)
+    data = rank_doc("test test test")
+    assert data.Document[0] == "TEst, test, test ,test ,test, test."
+
 
 def test_add_document_text():
     dummy_doc = Document(text="Dummy text.")
